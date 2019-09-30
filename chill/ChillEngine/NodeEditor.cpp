33d96d8558cdf3f336ab57eef46b492b4a296953
Pierre Bedell <pierre.bedell@gmail.com>
2019-09-30 17:07:01 +0200

    sdl2 integration

--- chill/ChillEngine/NodeEditor.cpp
+++ chill/ChillEngine/NodeEditor.cpp
@@ -9,6 +9,8 @@
 
 #include "SourcePath.h"
 
+#include <SDL.h>
+
 #ifdef USE_GLUT
 #include <GL/glut.h>
 #endif
@@ -17,6 +19,41 @@
 #define ForIndex(I, N) for(decltype(N) I=0; I<N; I++)
 
 //LIBSL_WIN32_FIX
+
+//-------------------------------------------------------
+// Test function for SDL2 integration
+int sdl_test() {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    std::cout << "Failed to initialize the SDL2 library\n";
+    return -1;
+  }
+
+  SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    680, 480,
+    0);
+
+  if (!window)
+  {
+    std::cout << "Failed to create window\n";
+    return -1;
+  }
+
+  SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+  if (!window_surface)
+  {
+    std::cout << "Failed to get the surface from the window\n";
+    return -1;
+  }
+
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Delay(5000);
+}
+//-------------------------------------------------------
 
 namespace chill
 {
@@ -1649,6 +1686,8 @@ namespace chill
   //-------------------------------------------------------
   void NodeEditor::launch()
   {
+    // SDL2 integration test
+    sdl_test();
 
     NodeEditor *nodeEditor = Instance();
 
